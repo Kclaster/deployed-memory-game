ad0c07ccb442ec7cc14a0e9bffa38b49ec4c3209
Kyle Laster <kyle.laster@gmail.com>
2019-04-26 22:52:01 -0500

    Updates

---
+++ precache-manifest.b19aeea5df6fc937b04b70951ca70305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80908464061133e6f54a937ae4b01927",
+    "url": "/deployed-memory-game/index.html"
+  },
+  {
+    "revision": "66a474059ba862a37a0a",
+    "url": "/deployed-memory-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/deployed-memory-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "66a474059ba862a37a0a",
+    "url": "/deployed-memory-game/static/js/main.278cb3e5.chunk.js"
+  },
+  {
+    "revision": "16a79671b2944be40964",
+    "url": "/deployed-memory-game/static/js/runtime~main.92f0ab53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployed-memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
